af5698b49fe0682e3e58d09f102d3b34931faaae
rtthw <172864013+rtthw@users.noreply.github.com>
2026-06-02 08:19:17 -0400

    Implement workaround for #2 in input driver

--- example/apps/input-driver/src/input_driver.rs
+++ example/apps/input-driver/src/input_driver.rs
@@ -16,17 +16,14 @@ pub extern "C" fn main() -> ! {
         ALLOCATOR.init(heap::BASE_ADDR, heap::DEFAULT_SIZE);
     }
 
-    // FIXME: For some reason, using both the keyboard and mouse here causes a
-    //        zero-sized buffer to be sent to one of the devices. This will poll
-    //        both devices when I figure out what's going on.
-    let mut virtio_inputs = Vec::with_capacity(1);
-    virtio_inputs.push(
+    // HACK: This is a workaround for not being able to use `Iterator::collect` in
+    //       userspace. See https://github.com/rtthw/rhubarb/issues/2 for more.
+    let mut virtio_inputs = Vec::with_capacity(2);
+    virtio_inputs.extend(
         pci::enumerate_devices()
             .into_iter()
             .filter(|dev| dev.vendor_id == 0x1af4 && dev.device_id == 0x1040 + 18)
-            .skip(1) // Handle mouse only, for now.
-            .find_map(|pci_device| virtio_input::Device::new(pci_device).ok())
-            .unwrap(),
+            .filter_map(|pci_device| virtio_input::Device::new(pci_device).ok()),
     );
 
     log::info!("Input driver starting...");
